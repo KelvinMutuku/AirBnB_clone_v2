699c71625ebe3920e5aefdce300a99e4b21d9282
bekiassefa <118681825+bekiassefa@users.noreply.github.com>
2023-06-21 07:52:47 -0700

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
-# Bash script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# Bash script that sets up your web servers for the deployment of web_static
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+apt-get -y update > /dev/null
+apt-get install -y nginx > /dev/null
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create all necessary directories and file
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo "Hello World again!" > /data/web_static/releases/test/index.html
 
-sudo chown -R ubuntu:ubuntu /data/
+# Check if directory current exist
+if [ -d "/data/web_static/current" ]
+then
+        sudo rm -rf /data/web_static/current
+fi
+# Create a symbolic link to test
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Change ownership to user ubuntu
+chown -hR ubuntu:ubuntu /data
 
-sudo service nginx restart
+# Configure nginx to serve content pointed to by symbolic link to hbnb_static
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart server
+service nginx restart
