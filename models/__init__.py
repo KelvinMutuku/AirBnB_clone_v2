19b2ce8eb516ffdaf55070919f76a59f0ca34c41
KelvinMutuku <mutukuk553@gmail.com>
2023-06-22 10:22:38 +0300

    models/base_model.py, models/city.py, models/state.py, models/engine/db_storage.py, models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an instance of the Storage will be used"""
+"""Instantiates a storage object.
 
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
 from os import getenv
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
 
-if storage_type == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
